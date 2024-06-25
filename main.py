1d674ddf653931d85b1a95cfb99734841ea02f88
Alexandra <s-kella@bk.ru>
2024-06-25 08:49:09 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import random
+import time
+import curses
+import asyncio
+
+
+def draw(canvas):
+    max_x, max_y = canvas.getmaxyx()
+    canvas.nodelay(True)
+    curses.curs_set(False)
+    coroutines = [fire(canvas, max_x // 2, max_y // 2)]
+
+    for _ in range(10):
+        symbol = random.choice('+*.:')
+        x = random.randint(0, max_x - 1)
+        y = random.randint(0, max_y - 1)
+        coroutines.append(blink(canvas, x, y, symbol))
+    while True:
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+                canvas.refresh()
+            except StopIteration:
+                coroutines.remove(coroutine)
+        time.sleep(0.1)
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+        for _ in range(random.randint(0, 20)):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in range(5):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
+
